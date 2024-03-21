f30ccd27059ac105e08bb4cabdd413d0366f76c4
FETHI Abderrahmane <fethiabderrahmane1@gmail.com>
2024-03-21 01:23:25 +0000

    adding receiving emails logic

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,30 +1,72 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import './contact.css'
 import Lottie from 'lottie-react'
-import emailjs from '@emailjs/browser';
-import contactAnimation from '../../../public/contact.json'
-// import contactAnimation from '../../../public/contact1.json'
+import { useForm, ValidationError } from '@formspree/react';
+import contactAnimation from '../../../src/contact.json'
+import doneAnimation from '../../../src/done.json'
+import errorAnimation from '../../../src/error.json'
 
 export default function Contact({ lightMode }) {
     const inputClassName = `${lightMode ? 'Light' : 'Dark'}`;
     const labelClassName = `${inputClassName}-name`;
+
+    const [state, handleSubmit] = useForm("meqylrzn");
+
     return (
-        <div className='container'>
-            <form>
-                <label className={labelClassName}>Name</label>
-                <input autoComplete='off' required type="text" name="user_name" className={inputClassName} />
-                <label className={labelClassName}>Email</label>
-                <input autoComplete='off' required type="email" name="user_email" className={inputClassName} />
-                <label className={labelClassName}>Message</label>
-                <textarea name="message" className={inputClassName} />
-                <input type="submit" value="Send" />
-            </form>
-            <div className='animation'>
-                <Lottie
-                    style={{height: 400}}
-                    animationData={contactAnimation}
-                />
+        <>
+            <div id='contact' className={`contact-me ${lightMode ? 'primary' : 'secondary'}`}>
+                Contact Me
             </div>
-        </div>
+            <div className='contact-container'>
+                <form onSubmit={handleSubmit}>
+                    <label className={labelClassName}>Name</label>
+                    <input autoComplete='off' required type="text" name="name" className={inputClassName} />
+                    <ValidationError
+                        prefix="Name"
+                        field="name"
+                        errors={state.errors}
+                    />
+                    <label className={labelClassName}>Email</label>
+                    <input autoComplete='off' required type="email" name="email" className={inputClassName} />
+                    <ValidationError
+                        prefix="Email"
+                        field="email"
+                        errors={state.errors}
+                    />
+                    <label className={labelClassName}>Message</label>
+                    <textarea name="message" className={inputClassName} />
+                    <ValidationError
+                        prefix="Message"
+                        field="message"
+                        errors={state.errors}
+                    />
+                    <button className={`submit-btn ${lightMode ? 'light-shadow' : 'dark-shadow'}`} type='submit' disabled={state.submitting}>
+                        {state.submitting ? 'Submitting ...' : 'Submit'}
+                    </button>
+                    {state.succeeded && (
+                        <p className={`paragraph-animation ${lightMode ? 'primary' : 'secondary'}`} style={{ fontSize: '15px' }}>
+                            <Lottie
+                                loop={false}
+                                style={{height: 25}}
+                                animationData={doneAnimation}
+                            />
+                            Your message has been sent successfully 😀</p>)}
+                    {state.errors && (
+                        <p className={`paragraph-animation ${lightMode ? 'primary' : 'secondary'}`} style={{ fontSize: '15px' }}>
+                            <Lottie
+                                loop={false}
+                                style={{ height: 33}}
+                                animationData={errorAnimation}
+                            />
+                            Ooops! Something went wrong</p>)}
+                </form>
+                <div className='animation'>
+                    <Lottie
+                        style={{ height: 400 }}
+                        animationData={contactAnimation}
+                    />
+                </div>
+            </div>
+        </>
     );
 }
