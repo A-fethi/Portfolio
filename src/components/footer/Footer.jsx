36f93bd54069cb6a24de27977de9afad6a2062f3
FETHI Abderrahmane <fethiabderrahmane1@gmail.com>
2024-03-21 15:31:36 +0000

    creating footer and making it responsive

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import { FaInstagram, FaLinkedin, FaGithub } from "react-icons/fa";
+import { FaXTwitter } from "react-icons/fa6";
 import './footer.css'
 
-export default function Footer() {
+export default function Footer({ lightMode }) {
     return (
-        <div></div>
+        <footer className={`footer ${lightMode ? 'footer-light' : 'footer-dark'}`}>
+            <ul className='footer-links'>
+                <li><a href="https://github.com/A-fethi"><FaGithub /></a></li>
+                <li><a href="https://www.linkedin.com/in/abderrahmane-fethi"><FaLinkedin /></a></li>
+                <li><a href="https://instagram.com/abderrahmane_fethi"><FaInstagram /></a></li>
+                <li><a href="https://twitter.com/AbduFet49"><FaXTwitter /></a></li>
+            </ul>
+            <p>&copy; 2024 Abderrahmane Fethi</p>
+        </footer>
     )
 }
