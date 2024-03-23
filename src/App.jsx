dfcb6732c65d15eccade8a172ecbf060262a5b54
FETHI Abderrahmane <fethiabderrahmane1@gmail.com>
2024-03-23 15:38:41 +0000

    Adding up-arrow to return to the top of the page

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import Header from './components/header/Header'
 import Hero from './components/hero/Hero'
 import Main from './components/main/Main'
@@ -6,11 +6,35 @@ import Contact from './components/contact/Contact'
 import Footer from './components/footer/Footer'
 
 function App() {
-  const [lightMode, setLightMode] = useState(true);
+  const [lightMode, setLightMode] = useState(null);
+  const [scroll, setScroll] = useState(false);
+
+  useEffect(() => {
+    const data = localStorage.getItem('lightMode');
+    if (data) {
+      setLightMode(JSON.parse(data));
+    }
+  }, []);
+
+  useEffect(() => {
+    if (lightMode !== null) {
+      localStorage.setItem('lightMode', JSON.stringify(lightMode));
+    }
+  }, [lightMode]);
 
   const toggleMode = () => {
     setLightMode(!lightMode);
-  }
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', () => {
+      if (window.scrollY > 300) {
+        setScroll(!scroll);
+      } else {
+        setScroll(scroll);
+      }
+    })
+  }, [])
 
   return (
     <div className='App'>
@@ -19,6 +43,9 @@ function App() {
       <Main lightMode={lightMode} />
       <Contact lightMode={lightMode} />
       <Footer lightMode={lightMode} />
+      <a href="#" style={{ opacity: scroll ? '1' : '0', transition: '.3s' }}>
+        <img className='up-arrow' src="/up-arrow.png" alt="" />
+      </a>
     </div>
   )
 }
