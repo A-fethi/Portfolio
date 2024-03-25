04b15051d790c25ba15c3b429d2bcdb49c3876cd
FETHI Abderrahmane <fethiabderrahmane1@gmail.com>
2024-03-25 14:23:24 +0000

    making anchors items to be opened in a new tab

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -6,10 +6,10 @@ export default function Footer({ lightMode }) {
     return (
         <footer className={`footer ${lightMode ? 'footer-light' : 'footer-dark'}`}>
             <ul className='footer-links'>
-                <li><a href="https://github.com/A-fethi"><FaGithub /></a></li>
-                <li><a href="https://www.linkedin.com/in/abderrahmane-fethi"><FaLinkedin /></a></li>
-                <li><a href="https://instagram.com/abderrahmane_fethi"><FaInstagram /></a></li>
-                <li><a href="https://twitter.com/AbduFet49"><FaXTwitter /></a></li>
+                <li><a href="https://github.com/A-fethi" target="_blank"><FaGithub /></a></li>
+                <li><a href="https://www.linkedin.com/in/abderrahmane-fethi" target="_blank"><FaLinkedin /></a></li>
+                <li><a href="https://instagram.com/abderrahmane_fethi" target="_blank"><FaInstagram /></a></li>
+                <li><a href="https://twitter.com/AbduFet49" target="_blank"><FaXTwitter /></a></li>
             </ul>
             <p>&copy; 2024 Abderrahmane Fethi</p>
         </footer>
